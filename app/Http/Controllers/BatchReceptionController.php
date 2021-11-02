55507cc684950919498d4c84fa81f40099b066b6
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-11-02 13:11:24 +0700

    get batch reception

--- app/Http/Controllers/BatchReceptionController.php
+++ app/Http/Controllers/BatchReceptionController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use JWTAuth;
+use App\Models\Transaction;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\DB;
 
 class BatchReceptionController extends Controller
 {
@@ -11,9 +18,42 @@ class BatchReceptionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $BatchReception = DB::table('ktv_tc_supplychain_batch AS sb')
+        ->select(
+        DB::raw('IF(SUM(st.VolumeNetto) IS NULL, FORMAT(0,2), SUM(st.VolumeNetto)) AS VolumeNetto,vso.Name AS SupplyOrgName'),
+        'sb.*'
+        )
+        ->leftJoin('view_tc_supplychain_org AS vso', 'vso.SupplychainID', '=', 'sb.SupplyOrgID')
+        ->leftJoin('view_tc_supplychain_org AS vso1', 'vso1.SupplychainID', '=', 'sb.SupplyDestMillOrgID')
+        ->leftJoin('ktv_tc_supplychain_transaction AS st', 'st.SupplyBatchID', '=', 'sb.SupplyBatchID')
+        ->where('sb.StatusCode', '=', 'active')
+        ->where('sb.SupplyBatchStatus', '=', 'Sent')
+        ->orWhere('sb.SupplyOrgID', '=', $request->SID)
+        ->orWhere('sb.SupplyDestMillOrgID', '=', $request->SID)
+        ->orWhere('sb.SupplyDestDoOrgID', '=', $request->SID)
+        ->orderByDesc('sb.SupplyBatchID')
+        ->get();
+
+        $Total = $BatchReception->count();
+
+        if($BatchReception) {
+            foreach ($BatchReception as $key => $value) {
+                $BatchReception[$key]->Trans = DB::table('ktv_tc_supplychain_transaction')
+                ->where('StatusCode', '=', 'active' )
+                ->where('SupplyBatchID', '=', $value->SupplyBatchID)
+                ->get();
+            }
+        }
+
+        //make response JSON
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil Ditampilkan',
+            'total'   => $Total,
+            'data'    => $BatchReception  
+        ], 200);
     }
 
     /**
