40889d9b05746cce58370fc0951a845743341498
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-12-04 12:30:04 +0700

    implementation redis get transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Cache;
 
 class TransactionController extends Controller
 {
@@ -20,74 +20,82 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        $transaction = DB::table('ktv_tc_supplychain_transaction AS ktst')
-        ->select(
-            'ktst.SupplyTransID',
-            'ktst.SupplychainID',
-            'ktst.SupplyBatchID',
-            'ktst.TransNumber',
-            'ktst.InvoiceNumber',
-            'ktst.DateTransaction',
-            'ktst.SupplyType',
-            'ktst.SupplyID',
-            'ktsb.SupplyDestType',
-            'ktsb.SupplyDestProcessType',
-            'ktst.PlantationNr',
-            'ktst.VolumeBruto',
-            'ktst.VolumeNetto',
-            'ktst.VolumeCutting',
-            'ktst.PackageID',
-            'ktst.PackageNumber',
-            'ktst.PackageWeight',
-            'ktst.DetailTypeID',
-            'ktst.TransStatusID',
-            'ktst.ContractPrice',
-            'ktst.NetPrice',
-            'ktst.DiscountPrice',
-            'ktst.TotalPayment',
-            'ktst.PaymentReduction',
-            'ktst.PaymentPaid',
-            'ktst.Longitude',
-            'ktst.Latitude',
-            'ktst.Notes',
-            'ktst.ChangeLog',
-            'ktst.ChangeBy',
-            'ktst.DateCreated',
-            'ktst.CreatedBy',
-            'ktst.DateUpdated',
-            'ktst.LastModifiedBy',
-            'ktst.DOID',
-            'ktst.AgentID',
-            'ktst.AgentOther',
-            'ktst.AgentOtherNik',
-            'ktst.AgentOtherSurvey',
-            'ktst.SupplyBatchType',
-            'ktst.MillID',
-            'ktst.MillOther',
-            'ktst.DOOther',
-            'ktst.SupplyBatchSourceType',
-            'ktst.DeductionPercentage',
-            'ktst.DeductionWeight',
-            DB::raw('IFNULL(ktst.Bunches,0) AS Bunches'),
-            'ktst.CollectpointID',
-            'ktst.AutoTransNumber',
-            'ktst.isTraceable'
-        )
-        ->leftJoin('ktv_trace_package AS ktp', 'ktp.PackageID', '=', 'ktst.PackageID')
-        ->leftJoin('ktv_tc_supplychain_batch AS ktsb', 'ktsb.SupplyBatchID', '=', 'ktst.SupplyBatchID')
-        ->where('ktst.StatusCode', '=', 'active')
-        ->where('ktst.SupplychainID', '=', '767')
-        ->get();
+        $value = Cache::rememberForever('transaction', function () {
+            
+            $transaction = DB::table('ktv_tc_supplychain_transaction AS ktst')
+            ->select(
+                'ktst.SupplyTransID',
+                'ktst.SupplychainID',
+                'ktst.SupplyBatchID',
+                'ktst.TransNumber',
+                'ktst.InvoiceNumber',
+                'ktst.DateTransaction',
+                'ktst.SupplyType',
+                'ktst.SupplyID',
+                'ktsb.SupplyDestType',
+                'ktsb.SupplyDestProcessType',
+                'ktst.PlantationNr',
+                'ktst.VolumeBruto',
+                'ktst.VolumeNetto',
+                'ktst.VolumeCutting',
+                'ktst.PackageID',
+                'ktst.PackageNumber',
+                'ktst.PackageWeight',
+                'ktst.DetailTypeID',
+                'ktst.TransStatusID',
+                'ktst.ContractPrice',
+                'ktst.NetPrice',
+                'ktst.DiscountPrice',
+                'ktst.TotalPayment',
+                'ktst.PaymentReduction',
+                'ktst.PaymentPaid',
+                'ktst.Longitude',
+                'ktst.Latitude',
+                'ktst.Notes',
+                'ktst.ChangeLog',
+                'ktst.ChangeBy',
+                'ktst.DateCreated',
+                'ktst.CreatedBy',
+                'ktst.DateUpdated',
+                'ktst.LastModifiedBy',
+                'ktst.DOID',
+                'ktst.AgentID',
+                'ktst.AgentOther',
+                'ktst.AgentOtherNik',
+                'ktst.AgentOtherSurvey',
+                'ktst.SupplyBatchType',
+                'ktst.MillID',
+                'ktst.MillOther',
+                'ktst.DOOther',
+                'ktst.SupplyBatchSourceType',
+                'ktst.DeductionPercentage',
+                'ktst.DeductionWeight',
+                DB::raw('IFNULL(ktst.Bunches,0) AS Bunches'),
+                'ktst.CollectpointID',
+                'ktst.AutoTransNumber',
+                'ktst.isTraceable'
+            )
+            ->leftJoin('ktv_trace_package AS ktp', 'ktp.PackageID', '=', 'ktst.PackageID')
+            ->leftJoin('ktv_tc_supplychain_batch AS ktsb', 'ktsb.SupplyBatchID', '=', 'ktst.SupplyBatchID')
+            ->where('ktst.StatusCode', '=', 'active')
+            ->where('ktst.SupplychainID', '=', '767')
+            ->get();
+    
+            $total = $transaction->count();
+            
+        });
 
-        $total = $transaction->count();
-         
-       //make response JSON
-       return response()->json([
-           'success' => true,
-           'message' => 'Data Berhasil Ditampilkan',
-           'total'   => $total,
-           'data'    => $transaction  
-       ], 200);
+        if($value){
+            return response()->json([
+                'data'    => $value->original
+            ]);
+        } else {
+            return response()->json([
+                'success' => failed,
+                'message' => 'Data tidak di temukan',
+                'code'    => 401  
+            ]);
+        }
     }
 
     /**
@@ -96,50 +104,105 @@ class TransactionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    // public function store(Request $request)
-    // {
-    //    //set validation
-    //    $validator = Validator::make($request->all(), [
-    //         'uuid'   => 'required',
-    //         'user_id' => 'required',
-    //         'device_timestamp' => 'required',
-    //         'total_amount' => 'required',
-    //         'paid_amount' => 'required',
-    //         'payment_method' => 'required'
-    //     ]);
-    
-    //     //response error validation
-    //     if ($validator->fails()) {
-    //         return response()->json($validator->errors(), 400);
-    //     }
+    public function store(Request $request)
+    {
+        // var_dump($request);
+        // ddd($request);
+        $transaction = DB::table('ktv_tc_supplychain_transaction')
+        ->where('SupplyTransID', '>', '0')
+        ->where('SupplyID', '=', $request->SupplyID)
+        ->where('SupplyType', '=', $request->SupplyType)
+        ->get();
 
-    //     //save to database
-    //     $transaction = Transaction::create([
-    //         'uuid'   => $request->uuid,
-    //         'user_id' => $request->user_id,
-    //         'device_timestamp' => $request->device_timestamp,
-    //         'total_amount' => $request->total_amount,
-    //         'paid_amount' => $request->paid_amount,
-    //         'payment_method' => $request->payment_method,
-    //     ]);
+        if (DB::table('ktv_tc_supplychain_transaction')->where('SupplyID', '=', $request->SupplyID)->where('SupplyType', '=', $request->SupplyType)->where('SupplyTransID', 1)->exists()) {
+            //update
+            echo "update";
+            exit;
+        } else {
+            //insert
+            // echo "insert";
+            // exit;
 
-    //     //success save to database
-    //     if($transaction) {
+            // ddd($request->TransNumber);
+            unset($request->TransNumber);
+            $data['TransNumber'] = $this->_generateTransNumber($request->SupplychainID,$request->DateTransaction); 
+            
+            $getMembers = DB::table('ktv_members AS b')
+            ->leftJoin('ktv_tc_supplychain_farmer AS sf', 'sf.FarmerID', '=', 'b.MemberID')
+            ->where('b.MemberID', '=', $request->SupplyType)
+            ->get();
+            
+            if(!empty($Untraceable['MemberID'])){
+                foreach($Untraceable['MemberID'] as $k =>$v){
+                    $cekFarmUntraceable = array(
+                        'FarmerCategory' => $v['FarmerCategory'],
+                        'Latitude' => $v['Latitude'],
+                        'Longitude' => $v['Longitude'],
+                    );
+                }
+            }
+            
+            $dataUntraceable = $cekFarmUntraceable;
+            
+            if($dataUntraceable['FarmerCategory'] == 'Unmapped' && $dataUntraceable['Latitude'] == '' && $dataUntraceable['Longitude'] == ''){
+                $data['isTraceable'] = 'NO'; 
+            }elseif($dataUntraceable['FarmerCategory'] == 'Mapped' && $dataUntraceable['Latitude'] == '' && $dataUntraceable['Longitude'] == ''){
+                $data['isTraceable'] = 'NO'; 
+            }elseif($dataUntraceable['FarmerCategory'] == 'Mapped' && $dataUntraceable['Latitude'] != '' && $dataUntraceable['Longitude'] != ''){
+                $data['isTraceable'] = 'YES';
+            } else {
+                $data['isTraceable'] = 'YES';
+            }
 
-    //         return response()->json([
-    //             'success' => true,
-    //             'message' => 'transaction Created',
-    //             'data'    => $transaction  
-    //         ], 201);
+            $transaction = DB::table('users')->insert([
+                'email' => 'kayla@example.com',
+                'votes' => 0
+            ]);
 
-    //     } 
+            //success save to database
+            if($transaction) {
 
-    //     //failed save to database
-    //     return response()->json([
-    //         'success' => false,
-    //         'message' => 'transaction Failed to Save',
-    //     ], 409);
-    // }
+                return response()->json([
+                    'success' => true,
+                    'message' => 'transaction Created',
+                    'data'    => $transaction  
+                ], 201);
+
+            } 
+
+            //failed save to database
+            return response()->json([
+                'success' => false,
+                'message' => 'transaction Failed to Save',
+            ], 409);
+        }
+
+        //set validation
+        //    $validator = Validator::make($request->all(), [
+        //         'uuid'   => 'required',
+        //         'user_id' => 'required',
+        //         'device_timestamp' => 'required',
+        //         'total_amount' => 'required',
+        //         'paid_amount' => 'required',
+        //         'payment_method' => 'required'
+        //     ]);
+        
+        //response error validation
+        // if ($validator->fails()) {
+        //     return response()->json($validator->errors(), 400);
+        // }
+
+        //save to database
+        // $transaction = Transaction::create([
+        //     'uuid'   => $request->uuid,
+        //     'user_id' => $request->user_id,
+        //     'device_timestamp' => $request->device_timestamp,
+        //     'total_amount' => $request->total_amount,
+        //     'paid_amount' => $request->paid_amount,
+        //     'payment_method' => $request->payment_method,
+        // ]);
+        
+    }
 
     /**
      * Display the specified resource.
